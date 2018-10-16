683e92d63b8f97b0a8198f949d307fc960fb3dea
James <jameshaller27@gmail.com>
2018-10-16 01:05:29 -0400

    Add argparser w/ verbosity

--- src/des-sim.py
+++ src/des-sim.py
@@ -5,6 +5,7 @@ from encrypter import Encrypter
 from key_scheduler import KeyScheduler
 
 import logging
+import argparse
 
 log = logging.getLogger('des-sim')
 log.setLevel(logging.NOTSET)  # less detail -> NOTSET, INFO, DEBUG -> more detail
@@ -13,6 +14,17 @@ log.addHandler(hand)
 
 
 def main():
+    parser = argparse.ArgumentParser(prog='des-sim.py')
+    parser.add_argument("-v", "--verbosity", action="count",
+                        help="verbosity of output (v, vv)")
+    args = parser.parse_args()
+
+    if args.verbosity is not None:
+        if args.verbosity == 1:
+            log.setLevel(logging.INFO)
+        elif args.verbosity >= 2:
+            log.setLevel(logging.DEBUG)
+
     key = input('key > ')
     ks = KeyScheduler(key)
 
